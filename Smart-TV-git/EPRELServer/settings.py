1a8b46e056a496741f32828764691643d08914f0
charlie knox <charlieknox704@gmail.com>
2023-12-05 00:56:19 +0000

    corsheaders

--- Smart-TV-git/EPRELServer/settings.py
+++ Smart-TV-git/EPRELServer/settings.py
@@ -14,6 +14,7 @@ import os
 from pathlib import Path
 import mimetypes
 mimetypes.add_type("text/css", ".css", True)
+import corsheaders
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
